fe0ab77650cdc31ac2f56ac3e4737ef3d5b0e134
RahanaMadhu <91406989+RahanaMadhu@users.noreply.github.com>
2021-09-26 12:37:27 +0530

    Add files via upload

---
+++ Searching.java
@@ -0,0 +1,31 @@
+class Searching
+{
+static String censor(String text,
+		     String word)
+{
+String[] word_list = text.split("\\s+");
+String result = "";
+String stars = "";
+for (int i = 0; i < word.length(); i++)
+     stars += "#";
+int index = 0;
+for (String i : word_list)
+{
+if (i.compareTo(word) == 0)
+word_list[index] = stars;
+index++;
+}
+for (String i : word_list)
+     result += i + " ";
+return result;
+}
+public static void main(String[] args)
+{
+String extract = "kompetenzen is a computer science "+
+                     "center for developers. I am pursuing my " +
+                     "major in computer science. ";
+String cen = "computer";
+System.out.println(censor(extract, cen));
+}
+}
+	
